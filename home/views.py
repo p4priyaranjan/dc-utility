2ecffb5a9bf1442b6e05d84983280facdf6ce12c
PRIYARANJAN KUMAR <coolranjan.love@gmail.com>
2021-06-28 19:36:01 +0530

    First Commit

---
+++ home/views.py
@@ -0,0 +1,37 @@
+from django.contrib.auth import models
+from django.shortcuts import render,redirect
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate,logout,login
+from home.models import TotalEntry
+from datetime import datetime
+
+# Create your views here.
+
+def index(request):
+    if request.user.is_anonymous:
+        return redirect('/loginuser')
+
+    tot_entry=TotalEntry.objects.all()
+    print(tot_entry[0].day01fir)
+    context={
+        'total_entry':tot_entry,
+        'time':datetime.now()
+    }
+    return render(request,'index.htm',context)
+
+def loginuser(request):
+    if request.method=='POST':
+        username=request.POST.get('username')
+        pwd=request.POST.get('passwd')
+        print(username,pwd)
+        user=authenticate(username=username,password=pwd)
+        print(user)
+        if user is not None:
+            login(request,user)
+            return redirect('/')
+    else:
+        return render(request,'login.htm')
+
+def logoutuser(request):
+    logout(request)
+    return render(request,'login.htm')
